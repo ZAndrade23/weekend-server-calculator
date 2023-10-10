fc5e748638621a9c385b4f75655c713e026c4f58
Zach Andrade <zach_andrade@yahoo.com>
2023-10-10 00:07:10 -0500

    I finished base mode. I diddnt fufill the proper comits but i was working through this all weekend struggling with the symbol/math part of the server.

--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,26 @@
 console.log('script sourced');
+let symbol='';
+
+function getEquationsFromServer(){
+        
+        axios.get('/equations').then((response) => {
+    console.log('equations', response.data);
+    let equationsList = response.data;
+    let content = document.querySelector('#result');
+    content.innerHTML = '';
+    for (let equations of equationsList) {
+        content.innerHTML += `
+        <div>${equations.equation}<div>
+        `;
+    }
+        }).catch((error) => {
+        
+            console.log(error);
+            alert('somethin went wrong!');
+        })
+    }
+getEquationsFromServer();
+
 
 function sendEquationsToServer(event) {
     event.preventDefault();
@@ -6,17 +28,61 @@ function sendEquationsToServer(event) {
     let firstNumber = document.querySelector('#number-input').value;
     let secondNumber = document.querySelector('#number-input2').value;
     console.log(`The number ${firstNumber} applied to ${secondNumber} equals `);
-    document.querySelector('#number-input').value = '';
-    document.querySelector('#number-input2').value = '';
-        axios.post('/songs',
-    
-     
-    ).then((response) => {
+        axios.post('/equations', {
+            firstNumber: document.querySelector('#number-input').value ,
+            symbol: symbol,
+            secondNumber: document.querySelector('#number-input2').value,           
+          })
+.then((response) => {
         console.log('POST successful');
         
-        getSongs()
+       
     }).catch((error) => {
         console.log(error);
         alert('something went wrong');
     });
+
+
+    document.querySelector('#number-input').value = '';
+    document.querySelector('#number-input2').value = '';
+   getEquationsFromServer();
     }
+
+//sendEquationsToServer();
+    function divideBy() 
+{ 
+        num1 = document.getElementById("number-input").value;
+        num2 = document.getElementById("number-input2").value;
+        symbol='/';
+//document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function multiplyBy() 
+{ 
+        num1 = document.getElementById("number-input").value;
+        num2 = document.getElementById("number-input2").value;
+        symbol='*';
+//document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function addedBy() 
+{ 
+        num1 = document.getElementById("number-input").value;
+        num2 = document.getElementById("number-input2").value;
+        symbol='+';
+//document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function subtractedBy() 
+{ 
+        num1 = document.getElementById("number-input").value;
+        num2 = document.getElementById("number-input2").value;
+        symbol='-';
+//document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function clearInput() {
+    document.querySelector('#number-input').value = '';
+    document.querySelector('#number-input2').value = '';
+    
+}
--- server/server.js
+++ server/server.js
@@ -4,19 +4,66 @@ const express = require('express');
 const app = express();
 const PORT=5011;
 
-const arrayEquations = [
+const equationsList = [
+    { equation: '10 - 7 = 3'
+    },
+    {
+        equation: '6 + 9 = 15'
+    },
+    {
+        equation: '8 / 2 = 4'
+    }
 
 ];
+
+
+
 app.use(express.json());
 
 app.use(express.static('server/public'));
 
+app.get('/equations', (req, res) => {
+    //   
+//console.log(req); //what is the request? lot of data
+console.log('request made for /equations');
+res.send(equationsList);
+});
+
 app.post('/equations', (req, res) => {
     console.log(req.body);
-    let newEquation =req.body;
-    //
-    arrayEquations.push(newEquation);
-    console.log('the new array is', arrayEquations);
+    console.log('@ POST/equation')
+    //let newEquation = req.body;
+    let equationOut = 0;
+
+    if(req.body.symbol === '+'){
+        let solution = Number(req.body.firstNumber) + Number(req.body.secondNumber);
+        equationOut = solution;
+        console.log(equationOut)
+//equation.solution = solution;
+    }else if (req.body.symbol === '-' ){
+        let solution = Number (req.body.firstNumber) - Number (req.body.secondNumber);
+        equationOut = solution;
+        console. log (equationOut);
+       // equation.solution = solution;
+        }else if (req.body.symbol === '*'){
+            let solution = Number (req.body.firstNumber) * Number (req.body.secondNumber);
+            equationOut = solution;
+            console. log (equationOut);
+            //equation.solution = solution;
+        }else if(req.body.symbol === '/') {
+            let solution = Number (req.body.firstNumber) / Number (req.body.secondNumber);
+            equationOut = solution;
+        console. log (equationOut) ;
+        //equation.solution = solution;
+        }
+
+        let solutionString = req.body.firstNumber+' ' + req.body.symbol+' ' + req.body.secondNumber+' ' + '=' +' '+ equationOut
+       
+       
+    equationsList.push({
+        equation:solutionString 
+    });
+    console.log('the new array is', equationsList);
     res.sendStatus(201)
 });
 
